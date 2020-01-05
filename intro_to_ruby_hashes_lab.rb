77b40cd9f0b1a88b25a647a79b49d6a9a4e3eecc
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-05 05:21:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,53 +1,66 @@
-def base_hash = {:railroads []
-
-}
-
-end
-
-def monopoly_with_second_tier =
-  {:railroads => {:pieces => 4}
+def monopoly
+  monopoly = {
+    railroads: {}
   }
-
 end
 
-def monopoly_with_third_tier =
-  {:railroads => {:rent_in_dollars => {[]}
-  }
-  [:reading_railroad]["mortgage_value"]).to eq("$100")
-  
-end
-
-def monopoly={
-  :railroads => 
-  {
-    :pieces => 4,
-    :rent_in_dollars => 
-    {
-         :one_piece_owned  => 25, 
-         :two_piece_owned  => 50,
-         :three_piece_owned  => 100,
-         :four_piece_owned  => 200
-    },
-    :names => 
-    {
-    :reading_railroad  => 
-    {
-      "mortgage_value" => "$100"
-    }, 
-    :pennsylvania_railroad  => {
-      "mortgage_value" => "$200"
-    },
-    :b_and_o_railroad  => {
-      "mortgage_value" => "$400"
-    },
-    :shortline_railroad  => {
-    "mortgage_value" => "$800"
-     }
+def monopoly_with_second_tier
+  #copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+  monopoly = {
+    railroads: {
+      pieces: 4,
+      names: {},
+      rent_in_dollars: {}
     }
   }
-}
-          
-  
 end
 
+def monopoly_with_third_tier
+	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	monopoly = {
+    railroads: {
+      pieces: 4,
+      names: {
+        :reading_railroad => {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline => {}
+      },
+      rent_in_dollars: {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      }
+    }
+  }
+end
 
+def monopoly_with_fourth_tier
+  #copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+  monopoly = {
+    railroads: {
+      pieces: 4,
+      names: {
+        :reading_railroad => {
+          "mortgage_value" => "$100"
+          },
+        :pennsylvania_railroad => {
+          "mortgage_value" => "$200"
+          },
+          :b_and_o_railroad => {
+            "mortgage_value" => "$400"
+          },
+          :shortline => {
+            "mortgage_value" => "$800"
+          }
+      },
+      rent_in_dollars: {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      }
+    }
+  }
+end
